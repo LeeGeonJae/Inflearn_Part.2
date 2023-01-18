dbafb973bf24c9c63c1a6f147736b2e76e104726
LeeGeonJae <96504565+LeeGeonJae@users.noreply.github.com>
2023-01-18 17:52:44 +0900

    삼각형 띄우기 - Mesh - Shader - HLSL

--- Engine/EnginePch.h
+++ Engine/EnginePch.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // 각종 include
-#include <Windows.h>
+#include <windows.h>
 #include <tchar.h>
 #include <memory>
 #include <string>
@@ -30,18 +30,18 @@ using namespace Microsoft::WRL;
 #pragma comment(lib, "d3dcompiler")
 
 // 각종 typedef
-using int8		= __int8;
-using int16		= __int16;
-using int32		= __int32;
-using int64		= __int64;
-using uint8		= unsigned __int8;
-using uint16	= unsigned __int16;
-using uint32	= unsigned __int32;
-using uint64	= unsigned __int64;
-using Vec2		= XMFLOAT2;
-using Vec3		= XMFLOAT3;
-using Vec4		= XMFLOAT4;
-using Matrix	= XMMATRIX;
+using int8 = __int8;
+using int16 = __int16;
+using int32 = __int32;
+using int64 = __int64;
+using uint8 = unsigned __int8;
+using uint16 = unsigned __int16;
+using uint32 = unsigned __int32;
+using uint64 = unsigned __int64;
+using Vec2 = XMFLOAT2;
+using Vec3 = XMFLOAT3;
+using Vec4 = XMFLOAT4;
+using Matrix = XMMATRIX;
 
 enum
 {
@@ -50,13 +50,20 @@ enum
 
 struct WindowInfo
 {
-	HWND	hwnd;		// 출력 윈도우
-	int32	width;		// 너비
-	int32	height;		// 높이
-	bool	windowed;	// 창모드 or 전체화면
+	HWND	hwnd; // 출력 윈도우
+	int32	width; // 너비
+	int32	height; // 높이
+	bool	windowed; // 창모드 or 전체화면
 };
 
-class Engine;
-extern unique_ptr<Engine> GEngine;
+struct Vertex
+{
+	Vec3 pos;
+	Vec4 color;
+};
 
-void HelloEngine();
+#define DEVICE			GEngine->GetDevice()->GetDevice()
+#define CMD_LIST		GEngine->GetCmdQueue()->GetCmdList()
+#define ROOT_SIGNATURE	GEngine->GetRootSignature()->GetSignature()
+
+extern unique_ptr<class Engine> GEngine;
